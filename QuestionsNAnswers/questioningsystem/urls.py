2589284ed20c311cb1568df12c3436719558f67e
Dinesh <dineshkrishnareddy@gmail.com>
2014-09-14 10:04:26 +0530

    fb login complete

--- QuestionsNAnswers/questioningsystem/urls.py
+++ QuestionsNAnswers/questioningsystem/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, url
 
 __author__ = 'kishore-pc'
 
-urlpatterns=patterns('',
-    url('login', 'questioningsystem.views.fbLogin'),
+urlpatterns=patterns('questioningsystem.views',
+    url('login', 'fbLogin'),
+    url('api','apifblogin'),
     # url('(\w+)', ''),
     # url('add-question', ''),
 )
--- QuestionsNAnswers/questioningsystem/views.py
+++ QuestionsNAnswers/questioningsystem/views.py
@@ -1,6 +1,18 @@
 # Create your views here.
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
+import json
 
 def fbLogin(request):
-    context = {}
-    return render(request, 'login.html', context)
+    return render(request, 'login.html')
+
+@api_view(['POST', 'GET'])
+def apifblogin(request):
+    details = json.loads(requests.get("https://graph.facebook.com/me?access_token="+request.DATA['accesstoken']).content)
+    if "error" not in details:
+        name = details['name']
+        mailid = details['email']
+        return Response({'name':name}, 200)
+    return Response({'error':"Not a valid user"}, 400)
